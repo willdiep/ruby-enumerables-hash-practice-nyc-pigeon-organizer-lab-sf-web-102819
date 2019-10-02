50c7faee504b9fbaab6714f39d6938d45f77ad80
Will Diep <widiep@gmail.com>
2019-10-02 05:02:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_list = {}
+  data.each do |color_gender_lives, value|
+    value.each do |stats, all_names|
+      # p all_names
+      all_names.each do |name|
+        # p name
+        if new_pigeon_list[name] == nil
+          new_pigeon_list[name] = {}
+        end
+
+        if new_pigeon_list[name][color_gender_lives] == nil
+          new_pigeon_list[name][color_gender_lives] = []
+        end
+
+        new_pigeon_list[name][color_gender_lives].push(stats.to_s)
+
+      end
+    end
+  end
+  new_pigeon_list
 end
